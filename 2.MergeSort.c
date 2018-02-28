50cc097e32e99914224cd3def2ad2edbc3807c6a
Gabriel Wu <gabrielwu84@gmail.com>
2018-02-28 15:40:54 +0800

    Implemented sentinel in the merge algo.

--- 2.MergeSort.c
+++ 2.MergeSort.c
@@ -3,9 +3,12 @@
  **/
 #include <stdio.h>
 #include <stdlib.h> // for malloc
+// work out value to use as sentinel for int type, INT_MAX.
+#define INT_MAX  ((1 << (sizeof(int)*8 - 2)) - 1 + (1 << (sizeof(int)*8 - 2))) 
 
-#define N 10 // array length
-int x[N]={3,2,6,2,1,3,123,5,2,75};
+#define N 20 // array length
+int x[N]={67,92,7,52,45,78,61,36,8,27,74,14,34,79,41,55,88,64,85,14};
+int inf=INT_MAX; // set infinity to sentinel value 
 
 void merge(int* A, int p, int q, int r);
 void mergeSort(int* A, int p, int r);
@@ -33,14 +36,16 @@ void merge(int* A, int p, int q, int r){
     // p to q sorted, q+1 to r sorted.
     int n1=q-p+1;   // no of elements in L
     int n2=r-q;     // = r-(q+1)+1, the no of elements in R. 
-    int* L = (int*) malloc((n1) * sizeof(int));
-    int* R = (int*) malloc((n2) * sizeof(int));
+    int* L = (int*) malloc((n1+1) * sizeof(int)); // + 1 for the sentinel
+    int* R = (int*) malloc((n2+1) * sizeof(int)); // + 1 for the sentinel
     for(int i=0;i<n1;i++){
         L[i]=A[p+i];
     }
+    L[n1]=inf;
     for(int j=0;j<n2;j++){
         R[j]=A[q+1+j];
     }
+    R[n2]=inf;
     int i=0; int j=0;
     for(int k=p;k<=r;k++){
         if(L[i]<=R[j]){
@@ -56,6 +61,7 @@ void merge(int* A, int p, int q, int r){
 }
 
 void mergeSort(int* A, int p, int r){
+    // sort array A from index p to index r. 
     if(p<r){
         int q=(p+r)/2; 
         mergeSort(A,p,q);
